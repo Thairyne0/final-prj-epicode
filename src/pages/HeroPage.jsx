4a406743cc6425789b9e9799499fd6d47dd18891
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-02-19 12:12:55 +0100

    NavBar 0.5

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,9 +1,11 @@
 import MyHeroSection from "../components/MyHeroSection";
+import MyNavBar from "../components/MyNavBar";
 import MyNewsLetter from "../components/MyNewsLetter";
 
 export default function HeroPage() {
   return (
     <div className="bg-black pb-28">
+      <MyNavBar></MyNavBar>
       <MyHeroSection></MyHeroSection>
       <MyNewsLetter></MyNewsLetter>
     </div>
