14fb8a16504f2e9715f27b7b0ca8673cb475c032
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-02-25 23:38:33 +0100

    LogIn Page Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import HeroPage from "./pages/HeroPage";
+import MyLogInComponent from "./components/MyLogInComponent";
+// import HeroPage from "./pages/HeroPage";
+// import LogInPage from "./pages/LogInPage";
 // import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <HeroPage></HeroPage>
+      <MyLogInComponent></MyLogInComponent>
     </>
   );
 }
---
+++ src/pages/LogInPage.jsx
@@ -0,0 +1,7 @@
+import MyLogInComponent from "../components/MyLogInComponent";
+
+export default function LogInPage() {
+  <div>
+    <MyLogInComponent></MyLogInComponent>
+  </div>;
+}
