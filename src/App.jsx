9e6553642ea4307fd7a8e4be2d91097d7f189c7e
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-02-27 09:43:54 +0100

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import MyLogInComponent from "./components/MyLogInComponent";
+// import MyLogInComponent from "./components/MyLogInComponent";
 // import HeroPage from "./pages/HeroPage";
 // import LogInPage from "./pages/LogInPage";
-// import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <MyLogInComponent></MyLogInComponent>
+      <HomePage></HomePage>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,15 @@ export default function HomePage() {
         </nav>
         <MyWithButton></MyWithButton>
       </header>
+      <main>
+        <section className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-4 ">
+          <div className="bg-red-900 rounded-md p-6">CIAO 1</div>
+          <div className="bg-neutral-400 rounded-md sm:col-span-3 p-6">
+            CIAO 2
+          </div>
+        </section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
