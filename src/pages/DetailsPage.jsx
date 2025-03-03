0c69bb8a39b4cd69fa3574d3662e26689dd5ae3d
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-03-03 19:28:23 +0100

    Delete Carousel

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,4 +1,3 @@
-import MyCarousel from "../components/MyCarousel";
 import MyNavBar from "../components/MyRedNavBar";
 
 export default function DetailsPage() {
@@ -10,9 +9,7 @@ export default function DetailsPage() {
         </nav>
       </header>
       <main className="p-10 px-24">
-        <section className="bg-gray-50 rounded-md col-span-4 lg:col-span-3 p-6 shadow-md shadow-gray-400 mt-8">
-          <MyCarousel></MyCarousel>
-        </section>
+        <section className="bg-gray-50 rounded-md col-span-4 lg:col-span-3 p-6 shadow-md shadow-gray-400 mt-8"></section>
       </main>
     </div>
   );
