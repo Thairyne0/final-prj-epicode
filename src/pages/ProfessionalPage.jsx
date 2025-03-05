2905efb94b4d2a0d82eb185e122969e779872c79
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-03-05 17:02:46 +0100

    Update ProfessionalPage.jsx

--- src/pages/ProfessionalPage.jsx
+++ src/pages/ProfessionalPage.jsx
@@ -1,3 +1,14 @@
+import MyNavBar from "../components/MyRedNavBar";
+
 export default function ProfessionalPage() {
-  return <div></div>;
+  return (
+    <div className="bg-white pb-28">
+      <header>
+        <nav>
+          <MyNavBar></MyNavBar>
+        </nav>
+      </header>
+      <main></main>
+    </div>
+  );
 }
