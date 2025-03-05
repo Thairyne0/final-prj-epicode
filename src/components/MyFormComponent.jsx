3794390ef0d382d3cf9c5568ab432a505bacb04d
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-03-05 16:38:23 +0100

    Update MyFormComponent.jsx

--- src/components/MyFormComponent.jsx
+++ src/components/MyFormComponent.jsx
@@ -89,7 +89,7 @@ export default function MyFormComponent() {
                   <div className="mt-4 flex text-sm/6 text-gray-600">
                     <label
                       htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-red-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-700 focus-within:ring-offset-2 hover:text-red-700"
                     >
                       <span>Upload a file</span>
                       <input
@@ -293,7 +293,7 @@ export default function MyFormComponent() {
         </button>
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-red-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Save
         </button>
