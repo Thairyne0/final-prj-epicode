8697fb6b4982f244aed554b158c49e62c7c2b820
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-03-05 23:09:40 +0100

    Update MyProfessionalForm.jsx

--- src/components/MyProfessionalForm.jsx
+++ src/components/MyProfessionalForm.jsx
@@ -1,4 +1,3 @@
-import { PhotoIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 export default function MyProfessionalForm() {
@@ -6,61 +5,18 @@ export default function MyProfessionalForm() {
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base/7 font-semibold text-gray-900">
-            Prenotazione appuntamento
-          </h2>
-          <p className="mt-1 text-sm/6 text-gray-600">
-            Queste informazioni saranno visualizzate dal professionista per
-            poterti garantire il miglior servizio possibile.
+          <hr className="my-2"></hr>
+          <p className="mt-1 text-base text-gray-600">
+            Queste informazioni saranno visualizzate sulla tua pagina personale.
           </p>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="name"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Marca Veicolo
-              </label>
-              <div className="mt-2">
-                <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="BMW, Fiat, Audi, ..."
-                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="surname"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Modello Veicolo
-              </label>
-              <div className="mt-2">
-                <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <input
-                    id="surname"
-                    name="surname"
-                    type="text"
-                    placeholder="Punto, Ibiza, Polo, ..."
-                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-            </div>
-
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="col-span-full">
               <label
                 htmlFor="about"
                 className="block text-sm/6 font-medium text-gray-900"
               >
-                Descrivi il problema della tua auto
+                Descrivi te stesso e la tua professionalitá
               </label>
               <div className="mt-2">
                 <textarea
@@ -70,41 +26,6 @@ export default function MyProfessionalForm() {
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   defaultValue={""}
                 />
-              </div>
-            </div>
-
-            <div className="col-span-full">
-              <label
-                htmlFor="cover-photo"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Foto del problema della tua auto (opzionale)
-              </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div className="text-center">
-                  <PhotoIcon
-                    aria-hidden="true"
-                    className="mx-auto size-12 text-gray-300"
-                  />
-                  <div className="mt-4 flex text-sm/6 text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-red-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-700 focus-within:ring-offset-2 hover:text-red-700"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                      />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs/5 text-gray-600">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
-                </div>
               </div>
             </div>
           </div>
@@ -165,6 +86,23 @@ export default function MyProfessionalForm() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="name-workshop"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Nome Autofficina
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name-workshop"
+                  name="name-workshop"
+                  type="text"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
