4a7a0336e210ab428b665a211820479e8df94f3d
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2025-03-10 23:55:56 +0100

    Update MyProfessionalForm.jsx

--- src/components/MyProfessionalForm.jsx
+++ src/components/MyProfessionalForm.jsx
@@ -1,239 +1,240 @@
-import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+
+const token = localStorage.getItem("token");
+
+function getUserIdFromToken(token) {
+  if (!token) return null;
+
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = JSON.parse(atob(base64));
+
+    console.log(jsonPayload);
+
+    return jsonPayload.userId;
+  } catch (error) {
+    console.error("Errore nella decodifica del token:", error);
+    return null;
+  }
+}
+
+const userId = getUserIdFromToken(token);
 
 export default function MyProfessionalForm() {
+  const [formData, setFormData] = useState({
+    nome: "",
+    cognome: "",
+    descrizione: "",
+    nomeAzienda: "",
+    regione: "",
+    indirizzo: "",
+    citta: "",
+    provincia: "",
+    cap: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!userId) {
+      console.error("ID utente non trovato nel token!");
+      return;
+    }
+
+    const requestData = {
+      utenteId: userId,
+      ...formData,
+    };
+
+    console.log(requestData);
+
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/professionisti/create",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+
+      if (!response.ok) {
+        console.log(requestData);
+        throw new Error("Errore durante la registrazione");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <hr className="my-2"></hr>
           <p className="mt-1 text-base text-gray-600">
             Queste informazioni saranno visualizzate sulla tua pagina personale.
           </p>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="col-span-full">
-              <label
-                htmlFor="about"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Descrivi te stesso e la tua professionalitá
+              <label className="block text-sm font-medium text-gray-900">
+                Descrivi te stesso e la tua professionalità
               </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  defaultValue={""}
-                />
-              </div>
+              <textarea
+                name="descrizione"
+                rows={3}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.descrizione}
+              />
             </div>
           </div>
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base/7 font-semibold text-gray-900">
+          <h2 className="text-base font-semibold text-gray-900">
             Informazioni Personali
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label
-                htmlFor="first-name"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
+              <label className="block text-sm font-medium text-gray-900">
                 Nome
               </label>
-              <div className="mt-2">
-                <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="nome"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.nome}
+              />
             </div>
 
             <div className="sm:col-span-3">
-              <label
-                htmlFor="last-name"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
+              <label className="block text-sm font-medium text-gray-900">
                 Cognome
               </label>
-              <div className="mt-2">
-                <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="cognome"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.cognome}
+              />
             </div>
 
             <div className="sm:col-span-4">
-              <label
-                htmlFor="email"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
+              <label className="block text-sm font-medium text-gray-900">
                 Indirizzo Email
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="email"
+                type="email"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.email}
+              />
             </div>
 
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="name-workshop"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Nome Autofficina
+            <div className="sm:col-span-2">
+              <label className="block text-sm font-medium text-gray-900">
+                Regione
               </label>
-              <div className="mt-2">
-                <input
-                  id="name-workshop"
-                  name="name-workshop"
-                  type="text"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="regione"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.regione}
+              />
             </div>
 
             <div className="sm:col-span-3">
-              <label
-                htmlFor="country"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Regione
+              <label className="block text-sm font-medium text-gray-900">
+                Nome Azienda
               </label>
-              <div className="mt-2 grid grid-cols-1">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                >
-                  <option>Abruzzo</option>
-                  <option>Basilicata</option>
-                  <option>Calabria</option>
-                  <option>Campania</option>
-                  <option>Emilia-Romagna</option>
-                  <option>Friuli-Venezia Giulia</option>
-                  <option>Lazio</option>
-                  <option>Liguria</option>
-                  <option>Lombardia</option>
-                  <option>Marche</option>
-                  <option>Molise</option>
-                  <option>Piemonte</option>
-                  <option>Puglia</option>
-                  <option>Sardegna</option>
-                  <option>Sicilia</option>
-                  <option>Toscana</option>
-                  <option>Trentino-Alto Adige</option>
-                  <option>Umbria</option>
-                  <option>Valle d&#39;Aosta</option>
-                  <option>Veneto</option>
-                </select>
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                />
-              </div>
+              <input
+                name="nomeAzienda"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.nomeAzienda}
+              />
             </div>
 
-            <div className="col-span-full">
-              <label
-                htmlFor="street-address"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
+            <div className="sm:col-span-3">
+              <label className="block text-sm font-medium text-gray-900">
                 Indirizzo
               </label>
-              <div className="mt-2">
-                <input
-                  id="street-address"
-                  name="street-address"
-                  type="text"
-                  autoComplete="street-address"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2 sm:col-start-1">
-              <label
-                htmlFor="city"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Cittá
-              </label>
-              <div className="mt-2">
-                <input
-                  id="city"
-                  name="city"
-                  type="text"
-                  autoComplete="address-level2"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="indirizzo"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.indirizzo}
+              />
             </div>
 
             <div className="sm:col-span-2">
-              <label
-                htmlFor="region"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Stato / Provincia
+              <label className="block text-sm font-medium text-gray-900">
+                Città
               </label>
-              <div className="mt-2">
-                <input
-                  id="region"
-                  name="region"
-                  type="text"
-                  autoComplete="address-level1"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="citta"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.citta}
+              />
             </div>
 
             <div className="sm:col-span-2">
-              <label
-                htmlFor="postal-code"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
+              <label className="block text-sm font-medium text-gray-900">
+                Provincia
+              </label>
+              <input
+                name="provincia"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.provincia}
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label className="block text-sm font-medium text-gray-900">
                 CAP
               </label>
-              <div className="mt-2">
-                <input
-                  id="postal-code"
-                  name="postal-code"
-                  type="text"
-                  autoComplete="postal-code"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
+              <input
+                name="cap"
+                type="text"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 focus:outline-red-900"
+                onChange={handleChange}
+                value={formData.cap}
+              />
             </div>
           </div>
         </div>
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm/6 font-semibold text-gray-900">
-          Cancel
+        <button type="button" className="text-sm font-semibold text-gray-900">
+          Annulla
         </button>
         <button
           type="submit"
-          className="rounded-md bg-red-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-red-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700"
         >
-          Save
+          Salva
         </button>
       </div>
     </form>
